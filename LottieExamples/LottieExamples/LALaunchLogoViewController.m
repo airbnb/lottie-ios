2b7aaa0a22c2a32c6b3d8e0bf5426ea80094b631
Brandon Withrow <brandon.withrow@airbnb.com>
2017-01-17 18:15:17 -0800

    Added support for custom sublayers Also fixed a bug with shapelayers

---
+++ LottieExamples/LottieExamples/LALaunchLogoViewController.m
@@ -0,0 +1,37 @@
+//
+//  LALaunchLogoViewController.m
+//  LottieExamples
+//
+//  Created by Brandon Withrow on 1/17/17.
+//  Copyright © 2017 Brandon Withrow. All rights reserved.
+//
+
+#import "LALaunchLogoViewController.h"
+
+@interface LALaunchLogoViewController ()
+
+@end
+
+@implementation LALaunchLogoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
