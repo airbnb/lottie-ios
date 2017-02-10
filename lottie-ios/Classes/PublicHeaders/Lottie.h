c0f5fbf164892e67c28d4606a0b9d1a09c62bbe5
Brandon Withrow <brandon.withrow@airbnb.com>
2017-02-10 14:25:58 -0800

    Fixed header issue with Carthage

--- lottie-ios/Classes/PublicHeaders/Lottie.h
+++ lottie-ios/Classes/PublicHeaders/Lottie.h
@@ -5,9 +5,15 @@
 //  Created by brandon_withrow on 1/27/17.
 //
 //
-
+@import Foundation;
 #ifndef Lottie_h
 #define Lottie_h
+
+//! Project version number for Lottie.
+FOUNDATION_EXPORT double LottieVersionNumber;
+
+//! Project version string for Lottie.
+FOUNDATION_EXPORT const unsigned char LottieVersionString[];
 
 #import "LOTAnimationTransitionController.h"
 #import "LOTAnimationView.h"
