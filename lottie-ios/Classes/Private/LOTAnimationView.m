ef36771ffaa1dc6aed09bd5dd363f779fef5e518
Brandon Withrow <brandon.withrow@airbnb.com>
2017-02-17 18:03:47 -0800

    Move main layerContainer over to compLayer

--- lottie-ios/Classes/Private/LOTAnimationView.m
+++ lottie-ios/Classes/Private/LOTAnimationView.m
@@ -13,6 +13,7 @@
 #import "LOTHelpers.h"
 #import "LOTAnimationView_Internal.h"
 #import "LOTAnimationCache.h"
+#import "LOTCompositionLayer.h"
 
 @implementation LOTAnimationState {
   BOOL _needsAnimationUpdate;
@@ -169,23 +170,9 @@
 
 @end
 
-@interface LOTCustomChild : NSObject
-
-@property (nonatomic, strong) LOTView *childView;
-@property (nonatomic, weak) LOTLayerView *layer;
-@property (nonatomic, assign) LOTConstraintType constraint;
-
-@end
-
-@implementation LOTCustomChild
-
-@end
-
 @implementation LOTAnimationView {
-  NSDictionary *_layerMap;
-  NSDictionary *_layerNameMap;
-  NSMutableArray *_customLayers;
-  CALayer *_animationContainer;
+  CALayer *_timingLayer;
+  LOTCompositionLayer *_compLayer;
   CADisplayLink *_completionDisplayLink;
   BOOL hasFullyInitialized_;
 }
@@ -270,9 +257,8 @@
 #if TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR
 
 - (void)_initializeAnimationContainer {
-    _animationContainer = [CALayer new];
-    _animationContainer.masksToBounds = YES;
-    [self.layer addSublayer:_animationContainer];
+    _timingLayer = [CALayer new];
+    [self.layer addSublayer:_timingLayer];
     self.clipsToBounds = YES;
 }
 
@@ -280,9 +266,8 @@
 
 - (void)_initializeAnimationContainer {
     self.wantsLayer = YES;
-    _animationContainer = [CALayer new];
-    _animationContainer.masksToBounds = YES;
-    [self.layer addSublayer:_animationContainer];
+    _timingLayer = [CALayer new];
+    [self.layer addSublayer:_timingLayer];
 }
 
 #endif
@@ -291,7 +276,7 @@
   _sceneModel = model;
   [self _buildSubviewsFromModel];
   LOTAnimationState *oldState = _animationState;
-  _animationState = [[LOTAnimationState alloc] initWithDuration:_sceneModel.timeDuration layer:_animationContainer frameRate:_sceneModel.framerate];
+  _animationState = [[LOTAnimationState alloc] initWithDuration:_sceneModel.timeDuration layer:_timingLayer frameRate:_sceneModel.framerate];
 
   if (restoreAnimation && oldState) {
     [self setLoopAnimation:oldState.loopAnimation];
@@ -308,68 +293,15 @@
 }
 
 - (void)_buildSubviewsFromModel {
-  if (_customLayers) {
-    for (LOTCustomChild *child in _customLayers) {
-      [child.childView.layer removeFromSuperlayer];
+  if (_sceneModel) {
+    if (_compLayer) {
+      [_compLayer removeFromSuperlayer];
+      [_compLayer removeAllAnimations];
+      _compLayer = nil;
     }
-    _customLayers = nil;
-  }
-  
-  if (_layerMap) {
-    _layerMap = nil;
-    [_animationContainer removeAllAnimations];
-    [_animationContainer.sublayers makeObjectsPerformSelector:@selector(removeFromSuperlayer)];
-  }
-  
-  if (_layerNameMap) {
-    _layerNameMap = nil;
-  }
-  
-  _animationContainer.transform = CATransform3DIdentity;
-  _animationContainer.bounds = _sceneModel.compBounds;
-  
-  NSMutableDictionary *layerMap = [NSMutableDictionary dictionary];
-  NSMutableDictionary *layerNameMap = [NSMutableDictionary dictionary];
-  
-  NSArray *reversedItems = [[_sceneModel.layerGroup.layers reverseObjectEnumerator] allObjects];
-  
-  LOTLayerView *maskedLayer = nil;
-  for (LOTLayer *layer in reversedItems) {
-    LOTLayerView *layerView = [[LOTLayerView alloc] initWithModel:layer inLayerGroup:_sceneModel.layerGroup];
-    layerMap[layer.layerID] = layerView;
-    layerNameMap[layer.layerName] = layerView;
-    if (maskedLayer) {
-      maskedLayer.mask = layerView;
-      maskedLayer = nil;
-    } else {
-      if (layer.matteType == LOTMatteTypeAdd) {
-        maskedLayer = layerView;
-      }
-      [_animationContainer addSublayer:layerView];
-    }
-  }
-  _layerMap = layerMap;
-  _layerNameMap = layerNameMap;
-}
-
-- (void)_layoutCustomChildLayers {
-  if (!_customLayers.count) {
-    return;
-  }
-  
-  for (LOTCustomChild *child in _customLayers) {
-    switch (child.constraint) {
-      case LOTConstraintTypeAlignToLayer:
-        child.childView.frame = child.layer.bounds;
-        break;
-      case LOTConstraintTypeAlignToBounds: {
-        CGRect selfBounds = _animationContainer.frame;
-        CGRect convertedBounds = [child.childView.layer.superlayer convertRect:selfBounds fromLayer:self.layer];
-        child.childView.layer.frame = convertedBounds;
-      } break;
-      default:
-        break;
-    }
+    _compLayer = [[LOTCompositionLayer alloc] initWithLayerGroup:_sceneModel.layerGroup
+                                                      withBounds:_sceneModel.compBounds];
+    [_timingLayer addSublayer:_compLayer];
   }
 }
 
@@ -411,29 +343,7 @@
 
 - (void)addSubview:(LOTView *)view
       toLayerNamed:(NSString *)layer {
-  LOTConstraintType constraint = LOTConstraintTypeAlignToBounds;
-  LOTLayerView *layerObject = _layerNameMap[layer];
-  LOTCustomChild *newChild = [[LOTCustomChild alloc] init];
-  newChild.constraint = constraint;
-  newChild.childView = view;
-  
-  if (!layer) {
-    NSException* layerNotFoundExpection = [NSException exceptionWithName:@"LayerNotFoundException"
-                                                                  reason:@"The required layer was not specified."
-                                                                userInfo:nil];
-    @throw layerNotFoundExpection;
-  } else {
-    newChild.layer = layerObject;
-    [layerObject.superlayer insertSublayer:view.layer above:layerObject];
-    
-    view.layer.mask = layerObject;
-  }
-  
-  if (!_customLayers) {
-    _customLayers = [NSMutableArray array];
-  }
-  [_customLayers addObject:newChild];
-  [self _layoutCustomChildLayers];
+  [_compLayer addSublayer:view toLayerNamed:layer];
 }
 
 # pragma mark - Display Link
@@ -599,7 +509,7 @@
 
 - (void)_layout {
   if (!hasFullyInitialized_) {
-    _animationContainer.bounds = self.bounds;
+    _compLayer.bounds = self.bounds;
     return;
   }
 
@@ -632,12 +542,13 @@
 
   [CATransaction begin];
   [CATransaction setDisableActions:YES];
-  _animationContainer.transform = CATransform3DIdentity;
-  _animationContainer.bounds = _sceneModel.compBounds;
-  _animationContainer.transform = xform;
-  _animationContainer.position = centerPoint;
+  _compLayer.transform = CATransform3DIdentity;
+  _compLayer.bounds = _sceneModel.compBounds;
+  _compLayer.transform = xform;
+  _compLayer.position = centerPoint;
+  [_compLayer layoutCustomChildLayers];
   [CATransaction commit];
-  [self _layoutCustomChildLayers];
+  
 }
 
 @end
