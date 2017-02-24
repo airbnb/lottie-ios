4ef0489c423a377f2f9d247668de4c5540a4d384
Brandon Withrow <brandon.withrow@airbnb.com>
2017-02-24 15:40:38 -0800

    Updating to 1.5.0

--- Example/Pods/Target Support Files/Pods-Lottie-Example-MacOS/Pods-Lottie-Example-MacOS-umbrella.h
+++ Example/Pods/Target Support Files/Pods-Lottie-Example-MacOS/Pods-Lottie-Example-MacOS-umbrella.h
@@ -1,13 +1,5 @@
 #ifdef __OBJC__
 #import <Cocoa/Cocoa.h>
-#else
-#ifndef FOUNDATION_EXPORT
-#if defined(__cplusplus)
-#define FOUNDATION_EXPORT extern "C"
-#else
-#define FOUNDATION_EXPORT extern
-#endif
-#endif
 #endif
 
 
--- Example/Pods/Target Support Files/Pods-lottie-ios_Tests/Pods-lottie-ios_Tests-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-lottie-ios_Tests/Pods-lottie-ios_Tests-frameworks.sh
@@ -59,13 +59,8 @@ code_sign_if_enabled() {
   if [ -n "${EXPANDED_CODE_SIGN_IDENTITY}" -a "${CODE_SIGNING_REQUIRED}" != "NO" -a "${CODE_SIGNING_ALLOWED}" != "NO" ]; then
     # Use the current code_sign_identitiy
     echo "Code Signing $1 with Identity ${EXPANDED_CODE_SIGN_IDENTITY_NAME}"
-    local code_sign_cmd="/usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} ${OTHER_CODE_SIGN_FLAGS} --preserve-metadata=identifier,entitlements '$1'"
-
-    if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
-      code_sign_cmd="$code_sign_cmd &"
-    fi
-    echo "$code_sign_cmd"
-    eval "$code_sign_cmd"
+    echo "/usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} ${OTHER_CODE_SIGN_FLAGS} --preserve-metadata=identifier,entitlements \"$1\""
+    /usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} ${OTHER_CODE_SIGN_FLAGS} --preserve-metadata=identifier,entitlements "$1"
   fi
 }
 
@@ -87,6 +82,3 @@ strip_invalid_archs() {
   fi
 }
 
-if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
-  wait
-fi
