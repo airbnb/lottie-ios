97dd5273858f43f30b794db964f408e4e6be85ab
Brandon Withrow <brandon.withrow@airbnb.com>
2017-02-24 15:40:38 -0800

    Updating to 1.5.0

--- Example/Pods/Target Support Files/Pods-Lottie-Example/Pods-Lottie-Example-resources.sh
+++ Example/Pods/Target Support Files/Pods-Lottie-Example/Pods-Lottie-Example-resources.sh
@@ -18,9 +18,6 @@ case "${TARGETED_DEVICE_FAMILY}" in
   2)
     TARGET_DEVICE_ARGS="--target-device ipad"
     ;;
-  3)
-    TARGET_DEVICE_ARGS="--target-device tv"
-    ;;
   *)
     TARGET_DEVICE_ARGS="--target-device mac"
     ;;
