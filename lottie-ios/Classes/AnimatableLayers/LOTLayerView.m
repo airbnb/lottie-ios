97a6417be57be37dc986764a67c70215b43af617
Jay Moore <jmoore@ehealthinnovation.org>
2017-02-28 12:37:40 -0500

    add warnings: missing image, unsupported effects

--- lottie-ios/Classes/AnimatableLayers/LOTLayerView.m
+++ lottie-ios/Classes/AnimatableLayers/LOTLayerView.m
@@ -329,6 +329,8 @@
     UIImage *image = [UIImage imageNamed:components.firstObject];
     if (image) {
       _childContainerLayer.contents = (__bridge id _Nullable)(image.CGImage);
+    } else {
+        NSLog(@"%s: Warn: image not found: %@", __PRETTY_FUNCTION__, components.firstObject);
     }
   }
 }
