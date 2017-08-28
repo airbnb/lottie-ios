8a253be575b608a76982d4758bc88e84632994e0
brandon_withrow <brandon.withrow@airbnb.con>
2017-08-28 14:57:31 -0700

    Updated example

--- Example/lottie-ios/LAControlsViewController.m
+++ Example/lottie-ios/LAControlsViewController.m
@@ -37,18 +37,44 @@
   [toggle1 setProgressRangeForOffState:0 toProgress:0.5];
   
   [toggle1 addTarget:self action:@selector(switchToggled:) forControlEvents:UIControlEventValueChanged];
-  toggle1.center = CGPointMake(CGRectGetMidX(self.view.bounds), 90);
   [self.view addSubview:toggle1];
   
   /// An animated 'like' or 'heart' button.
   /// Clicking toggles the Like or Heart state.
   /// The animation runs from 0-1, progress 0 is off, progress 1 is on
   LOTAnimatedSwitch *heartIcon = [LOTAnimatedSwitch switchNamed:@"TwitterHeart"];
-  heartIcon.bounds = CGRectMake(0, 0, 200, 200);
-  heartIcon.center = CGPointMake(CGRectGetMidX(self.view.bounds), CGRectGetMaxY(toggle1.frame) + (heartIcon.bounds.size.height * 0.5));
+  
   [heartIcon addTarget:self action:@selector(switchToggled:) forControlEvents:UIControlEventValueChanged];
   [self.view addSubview:heartIcon];
   
+  /// This is a switch that also has a Disabled state animation.
+  /// When the switch is disabled then the disabled layer is displayed.
+  
+  LOTAnimatedSwitch *statefulSwitch = [LOTAnimatedSwitch switchNamed:@"Switch_States"];
+  
+  /// Off animation is 0 to 1 progress.
+  /// On animation is 1 to 0 progress.
+  [statefulSwitch setProgressRangeForOnState:1 toProgress:0];
+  [statefulSwitch setProgressRangeForOffState:0 toProgress:1];
+  
+  // Specify the layer names for different states
+  [statefulSwitch setLayerName:@"Button" forState:UIControlStateNormal];
+  [statefulSwitch setLayerName:@"Disabled" forState:UIControlStateDisabled];
+  
+  // Changes visual appearance by switching animation layer to "Disabled"
+  statefulSwitch.enabled = NO;
+  
+  // Changes visual appearance by switching animation layer to "Button"
+  statefulSwitch.enabled = YES;
+  
+  [statefulSwitch addTarget:self action:@selector(switchToggled:) forControlEvents:UIControlEventValueChanged];
+  [self.view addSubview:statefulSwitch];
+  
+  // Layout
+  toggle1.center = CGPointMake(CGRectGetMidX(self.view.bounds), 90);
+  heartIcon.bounds = CGRectMake(0, 0, 200, 200);
+  heartIcon.center = CGPointMake(CGRectGetMidX(self.view.bounds), CGRectGetMaxY(toggle1.frame) + (heartIcon.bounds.size.height * 0.5));
+  statefulSwitch.center = CGPointMake(CGRectGetMidX(self.view.bounds), CGRectGetMaxY(heartIcon.frame) + (statefulSwitch.bounds.size.height * 0.5));
 }
 
 - (void)switchToggled:(LOTAnimatedSwitch *)animatedSwitch {
