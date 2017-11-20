34604b11183a383cb935470d5a9cef406a27669e
mlisivick <matt2014!>
2017-11-20 19:57:48 -0300

    Stop animation before leaving screen to prevent freezing

--- lottie-ios/Classes/Private/LOTAnimationView.m
+++ lottie-ios/Classes/Private/LOTAnimationView.m
@@ -562,6 +562,7 @@ static NSString * const kCompContainerAnimationKey = @"play";
   } else {
     // The view is being detached, capture information that need to be restored later.
     if (_isAnimationPlaying) {
+        [self stop];
       _shouldRestoreStateWhenAttachedToWindow = YES;
       _completionBlockToRestoreWhenAttachedToWindow = _completionBlock;
       _completionBlock = nil;
