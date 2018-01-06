399649d964e508dbeb8a80c35030f7cbafc3892e
brandon_withrow <brandon.withrow@airbnb.con>
2018-01-05 16:36:17 -0800

    Update example projects

--- Example/Pods/Target Support Files/lottie-ios-iOS/lottie-ios-iOS-umbrella.h
+++ Example/Pods/Target Support Files/lottie-ios-iOS/lottie-ios-iOS-umbrella.h
@@ -8,11 +8,14 @@
 #import "LOTAnimationTransitionController.h"
 #import "LOTAnimationView.h"
 #import "LOTAnimationView_Compat.h"
+#import "LOTBlockCallback.h"
 #import "LOTCacheProvider.h"
 #import "LOTComposition.h"
+#import "LOTInterpolatorCallback.h"
 #import "LOTKeypath.h"
 #import "Lottie.h"
 #import "LOTValueCallback.h"
+#import "LOTValueDelegate.h"
 
 FOUNDATION_EXPORT double LottieVersionNumber;
 FOUNDATION_EXPORT const unsigned char LottieVersionString[];
