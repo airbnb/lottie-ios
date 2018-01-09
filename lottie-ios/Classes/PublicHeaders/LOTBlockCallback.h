3734b32c6d34892cffae2841044f9bcbfdfc472d
brandon_withrow <brandon.withrow@airbnb.con>
2018-01-08 16:06:46 -0800

    Comment update

--- lottie-ios/Classes/PublicHeaders/LOTBlockCallback.h
+++ lottie-ios/Classes/PublicHeaders/LOTBlockCallback.h
@@ -13,8 +13,8 @@
 /*!
  @brief A block that is used to change a Color value at keytime, the block is called continuously for a keypath while the aniamtion plays.
  @param currentFrame The current frame of the animation in the parent compositions time space.
- @param startFrame When the block is called, startFrame is the most recent keyframe for the keypath in relation to the current time.
- @param endFrame When the block is called, endFrame is the next keyframe for the keypath in relation to the current time.
+ @param startKeyFrame When the block is called, startFrame is the most recent keyframe for the keypath in relation to the current time.
+ @param endKeyFrame When the block is called, endFrame is the next keyframe for the keypath in relation to the current time.
  @param interpolatedProgress A value from 0-1 that represents the current progress between keyframes. It respects the keyframes current easing curves.
  @param startColor The color from the previous keyframe in relation to the current time.
  @param endColor The color from the next keyframe in relation to the current time.
@@ -32,8 +32,8 @@ typedef CGColorRef _Nonnull (^LOTColorValueCallbackBlock)(CGFloat currentFrame,
 /*!
  @brief A block that is used to change a Number value at keytime, the block is called continuously for a keypath while the aniamtion plays.
  @param currentFrame The current frame of the animation in the parent compositions time space.
- @param startFrame When the block is called, startFrame is the most recent keyframe for the keypath in relation to the current time.
- @param endFrame When the block is called, endFrame is the next keyframe for the keypath in relation to the current time.
+ @param startKeyFrame When the block is called, startFrame is the most recent keyframe for the keypath in relation to the current time.
+ @param endKeyFrame When the block is called, endFrame is the next keyframe for the keypath in relation to the current time.
  @param interpolatedProgress A value from 0-1 that represents the current progress between keyframes. It respects the keyframes current easing curves.
  @param startValue The Number from the previous keyframe in relation to the current time.
  @param endValue The Number from the next keyframe in relation to the current time.
@@ -50,8 +50,8 @@ typedef CGFloat (^LOTNumberValueCallbackBlock)(CGFloat currentFrame,
 /*!
  @brief A block that is used to change a Point value at keytime, the block is called continuously for a keypath while the aniamtion plays.
  @param currentFrame The current frame of the animation in the parent compositions time space.
- @param startFrame When the block is called, startFrame is the most recent keyframe for the keypath in relation to the current time.
- @param endFrame When the block is called, endFrame is the next keyframe for the keypath in relation to the current time.
+ @param startKeyFrame When the block is called, startFrame is the most recent keyframe for the keypath in relation to the current time.
+ @param endKeyFrame When the block is called, endFrame is the next keyframe for the keypath in relation to the current time.
  @param interpolatedProgress A value from 0-1 that represents the current progress between keyframes. It respects the keyframes current easing curves.
  @param startPoint The Point from the previous keyframe in relation to the current time.
  @param endPoint The Point from the next keyframe in relation to the current time.
@@ -69,8 +69,8 @@ typedef CGPoint (^LOTPointValueCallbackBlock)(CGFloat currentFrame,
 /*!
  @brief A block that is used to change a Size value at keytime, the block is called continuously for a keypath while the aniamtion plays.
  @param currentFrame The current frame of the animation in the parent compositions time space.
- @param startFrame When the block is called, startFrame is the most recent keyframe for the keypath in relation to the current time.
- @param endFrame When the block is called, endFrame is the next keyframe for the keypath in relation to the current time.
+ @param startKeyFrame When the block is called, startFrame is the most recent keyframe for the keypath in relation to the current time.
+ @param endKeyFrame When the block is called, endFrame is the next keyframe for the keypath in relation to the current time.
  @param interpolatedProgress A value from 0-1 that represents the current progress between keyframes. It respects the keyframes current easing curves.
  @param startSize The Size from the previous keyframe in relation to the current time.
  @param endSize The Size from the next keyframe in relation to the current time.
@@ -88,8 +88,8 @@ typedef CGSize (^LOTSizeValueCallbackBlock)(CGFloat currentFrame,
 /*!
  @brief A block that is used to change a Path value at keytime, the block is called continuously for a keypath while the aniamtion plays.
  @param currentFrame The current frame of the animation in the parent compositions time space.
- @param startFrame When the block is called, startFrame is the most recent keyframe for the keypath in relation to the current time.
- @param endFrame When the block is called, endFrame is the next keyframe for the keypath in relation to the current time.
+ @param startKeyFrame When the block is called, startFrame is the most recent keyframe for the keypath in relation to the current time.
+ @param endKeyFrame When the block is called, endFrame is the next keyframe for the keypath in relation to the current time.
  @param interpolatedProgress A value from 0-1 that represents the current progress between keyframes. It respects the keyframes current easing curves.
  @return UIBezierPath the path to set the keypath node for the current frame.
  */
