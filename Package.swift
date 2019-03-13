9f962d9fa9746cc789e94d92e309778cae41ec51
Cihat Gündüz <commits@cihatguenduez.de>
2019-03-13 11:38:57 +0100

    Add basic SwiftPM manifest file

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "Lottie",
+    products: [
+        .library(name: "Lottie", targets: ["Lottie"])
+    ],
+    targets: [
+        .target(
+            name: "Lottie",
+            path: "lottie-ios/Classes"
+        )
+    ]
+)
