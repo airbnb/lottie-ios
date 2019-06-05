6427b0f1ffefc35296f5b16c7a6512345b72a61d
StatusQuo <bastihumann@googlemail.com>
2019-06-06 01:18:40 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,9 @@
-// swift-tools-version:4.2
+// swift-tools-version:5
 import PackageDescription
 
 let package = Package(
     name: "Lottie",
+    platforms: [.iOS(.v12)],
     // platforms: [.iOS("9.0"), .macOS("10.10"), tvOS("9.0"), .watchOS("2.0")],
     products: [
         .library(name: "Lottie", targets: ["Lottie"])
