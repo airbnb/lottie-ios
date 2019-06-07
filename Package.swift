cde106e6a1cd6bf7ea77355500cf7d5f20e9250d
CI <ci-mojo@macko.fr>
2019-06-07 18:03:32 +0200

    Fix typo

--- Package.swift
+++ Package.swift
@@ -2,23 +2,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "Lottie",
-    platforms: [.iOS(.v12)],
-    // platforms: [.iOS("9.0"), .macOS("10.10"), tvOS("9.0"), .watchOS("2.0")],
-    products: [
-        .library(name: "Lottie", targets: ["Lottie"])
-    ],
-    targets: [
-        .target(
-            name: "Lottie",
-            path: "lottie-swift/src",
-			exclude: [ "lottie-swift/src/MacOS" ]
-            sources: [
-				"lottie-swift/src/iOS",
-				"lottie-swift/src/Private",
-				"lottie-swift/src/Public"
-			]
-        )
-    ],
-    swiftLanguageVersions: [.v4_2, .v5]
+  name: "Lottie",
+  platforms: [.iOS(.v12)],
+  // platforms: [.iOS("9.0"), .macOS("10.10"), tvOS("9.0"), .watchOS("2.0")],
+  products: [
+    .library(name: "Lottie", targets: ["Lottie"])
+  ],
+  targets: [
+    .target(
+      name: "Lottie",
+      path: "lottie-swift/src",
+      exclude: [ "lottie-swift/src/MacOS" ],
+      sources: [
+        "lottie-swift/src/iOS",
+        "lottie-swift/src/Private",
+        "lottie-swift/src/Public"
+      ]
+    )
+  ],
+  swiftLanguageVersions: [.v4_2, .v5]
 )
