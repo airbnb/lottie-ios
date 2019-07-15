df84d9c1bdf5cec6056997ce081348569758f9b8
Tyler Hedrick <tyler.hedrick@airbnb.com>
2019-07-15 15:47:13 -0700

    Add cancelBehavior to AnimatedSwitch

--- lottie-swift/src/Public/iOS/AnimatedSwitch.swift
+++ lottie-swift/src/Public/iOS/AnimatedSwitch.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+
 /**
  An interactive switch with an 'On' and 'Off' state. When the user taps on the
  switch the state is toggled and the appropriate animation is played.
@@ -14,6 +15,13 @@ import UIKit
  Both the 'On' and 'Off' have an animation play range associated with their state.
  */
 final public class AnimatedSwitch: AnimatedControl {
+
+  /// Defines what happens when the user taps the switch while an
+  /// animation is still in flight
+  public enum CancelBehavior {
+    case reverse // default - plays the current animation in reverse
+    case none    // does not update the animation when canceled
+  }
   
   /// The current state of the switch.
   public var isOn: Bool {
@@ -26,6 +34,9 @@ final public class AnimatedSwitch: AnimatedControl {
       return _isOn
     }
   }
+
+  /// The cancel behavior for the switch. See CancelBehavior for options
+  public var cancelBehavior: CancelBehavior = .reverse
 
   /// Set the state of the switch and specify animation and haptics
   public func setIsOn(_ isOn: Bool, animated: Bool, shouldFireHaptics: Bool = true) {
@@ -109,16 +120,18 @@ final public class AnimatedSwitch: AnimatedControl {
     var startProgress = isOn ? onStartProgress : offStartProgress
     var endProgress = isOn ? onEndProgress : offEndProgress
     let finalProgress = endProgress
-    
-    let realtimeProgress = animationView.realtimeAnimationProgress
-    
-    let previousStateStart = isOn ? offStartProgress : onStartProgress
-    let previousStateEnd = isOn ? offEndProgress : onEndProgress
-    if realtimeProgress.isInRange(min(previousStateStart, previousStateEnd),
-                                  max(previousStateStart, previousStateEnd)) {
-      /// Animation is currently in the previous time range. Reverse the previous play.
-      startProgress = previousStateEnd
-      endProgress = previousStateStart
+
+    if cancelBehavior == .reverse {
+      let realtimeProgress = animationView.realtimeAnimationProgress
+
+      let previousStateStart = isOn ? offStartProgress : onStartProgress
+      let previousStateEnd = isOn ? offEndProgress : onEndProgress
+      if realtimeProgress.isInRange(min(previousStateStart, previousStateEnd),
+                                    max(previousStateStart, previousStateEnd)) {
+        /// Animation is currently in the previous time range. Reverse the previous play.
+        startProgress = previousStateEnd
+        endProgress = previousStateStart
+      }
     }
     
     guard animated == true else {
