8763243669ff81b57ccb3885388d11b12b57af5b
Viktor Radulov <golova@zeoalliance.com>
2019-10-22 16:52:11 +0300

    Add swift package for macos

--- Package.swift
+++ Package.swift
@@ -1,10 +1,25 @@
 // swift-tools-version:5.0
 import PackageDescription
 
+#if os(macOS)
+let package = Package(
+    name: "Lottie",
+    platforms: [.macOS("10.9")],
+    products: [
+        .library(name: "Lottie", targets: ["Lottie"])
+    ],
+    targets: [
+        .target(
+            name: "Lottie",
+            path: "lottie-swift/src",
+            exclude: ["Public/iOS"]
+        )
+    ]
+)
+#else
 let package = Package(
     name: "Lottie",
     platforms: [.iOS(.v9)],
-    // platforms: [.iOS("9.0"), .macOS("10.10"), tvOS("9.0"), .watchOS("2.0")],
     products: [
         .library(name: "Lottie", targets: ["Lottie"])
     ],
@@ -16,3 +31,4 @@ let package = Package(
         )
     ]
 )
+#endif
