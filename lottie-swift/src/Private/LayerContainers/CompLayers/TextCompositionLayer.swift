343eb7888cf7ce4717a2d4f102fd3e380c13723f
John Lima <thejohnlima@icloud.com>
2019-11-10 12:43:37 -0300

    Update project to Swift 5

--- lottie-swift/src/Private/LayerContainers/CompLayers/TextCompositionLayer.swift
+++ lottie-swift/src/Private/LayerContainers/CompLayers/TextCompositionLayer.swift
@@ -115,7 +115,7 @@ final class TextCompositionLayer: CompositionLayer {
     
     let text = textDocument.value(frame: frame) as! TextDocument
     let anchorPoint = interpolatableAnchorPoint?.value(frame: frame) as! Vector3D
-    let scale = interpolatableScale?.value(frame: frame) as! Vector3D
+    _ = interpolatableScale?.value(frame: frame) as! Vector3D
     rootNode?.rebuildOutputs(frame: frame)
     
     let fillColor = rootNode?.textOutputNode.fillColor ?? text.fillColorData.cgColorValue
