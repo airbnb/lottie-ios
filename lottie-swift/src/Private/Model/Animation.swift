b0b117b4f3a7dfedfecdf0f1a36bbdc21ebd4f7b
Viktor Radulov <golova@zeoalliance.com>
2020-12-10 17:41:22 +0200

    Fixed mobile promo

--- lottie-swift/src/Private/Model/Animation.swift
+++ lottie-swift/src/Private/Model/Animation.swift
@@ -104,9 +104,9 @@ public class Animation: Codable {
     self.fonts = try container.decodeIfPresent(FontList.self, forKey: .fonts)
     self.assetLibrary = try container.decodeIfPresent(AssetLibrary.self, forKey: .assetLibrary)
     if type == .type3d {
-//        for layer in layers where layer is PreCompLayerModel {
-//            layer.transform.scale = layer.transform.scale.flipLast()
-//        }
+        for layer in layers where layer is PreCompLayerModel {
+            layer.transform.scale = layer.transform.scale.flipLast()
+        }
 
         for asset in assetLibrary?.precompAssets ?? [:] {
             for layer in asset.value.layers where layer is PreCompLayerModel {
