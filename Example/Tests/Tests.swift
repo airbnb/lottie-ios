65350066f9732d9d7f59238312dc42ec71495c3f
Ferico Samuel <ferico.samuel@tokopedia.com>
2021-05-11 10:40:57 +0700

    lottie logo tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,15 +1,43 @@
+import SnapshotTesting
 import XCTest
 
+@testable import Lottie
+
 class Tests: XCTestCase {
-  
+  let animationView = AnimationView()
+
   override func setUp() {
     super.setUp()
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    animationView.contentMode = .scaleAspectFit
   }
-  
+
+  func testLottieLogo() {
+    isRecording = true
+    let animation = Animation.named("LottieLogo1", subdirectory: "TestAnimations")
+    animationView.animation = animation
+
+    animationView.currentProgress = 0.1
+    assertSnapshot(matching: animationView, as: .image(size: CGSize(width: 500, height: 500)))
+
+    animationView.currentProgress = 0.25
+    assertSnapshot(matching: animationView, as: .image(size: CGSize(width: 500, height: 500)))
+
+    animationView.currentProgress = 0.5
+    assertSnapshot(matching: animationView, as: .image(size: CGSize(width: 500, height: 500)))
+
+    animationView.currentProgress = 0.75
+    assertSnapshot(matching: animationView, as: .image(size: CGSize(width: 500, height: 500)))
+
+    animationView.currentProgress = 0.9
+    assertSnapshot(matching: animationView, as: .image(size: CGSize(width: 500, height: 500)))
+
+    animationView.currentProgress = 1.0
+    assertSnapshot(matching: animationView, as: .image(size: CGSize(width: 500, height: 500)))
+  }
+
   override func tearDown() {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     super.tearDown()
   }
-  
 }
