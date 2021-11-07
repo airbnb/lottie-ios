b2681adde554e8cc676be4d6e7339bb0e5d0c3d1
chizhavko <mchizhavko@mail.ru>
2021-11-07 19:37:08 +0300

    ISSUE #1412 - Cannot parse json in case of jpg sequences, lost indices while parsing

--- lottie-swift/src/Private/LayerContainers/Utility/CompositionLayersInitializer.swift
+++ lottie-swift/src/Private/LayerContainers/Utility/CompositionLayersInitializer.swift
@@ -22,18 +22,19 @@ extension Array where Element == LayerModel {
     var childLayers = [LayerModel]()
     
     for layer in self {
+      let layerIndex = layer.index ?? Int(layer.inFrame)
       if layer.hidden == true {
         let genericLayer = NullCompositionLayer(layer: layer)
         compositionLayers.append(genericLayer)
-        layerMap[layer.index] = genericLayer
+        layerMap[layerIndex] = genericLayer
       } else if let shapeLayer = layer as? ShapeLayerModel {
         let shapeContainer = ShapeCompositionLayer(shapeLayer: shapeLayer)
         compositionLayers.append(shapeContainer)
-        layerMap[layer.index] = shapeContainer
+        layerMap[layerIndex] = shapeContainer
       } else if let solidLayer = layer as? SolidLayerModel {
         let solidContainer = SolidCompositionLayer(solid: solidLayer)
         compositionLayers.append(solidContainer)
-        layerMap[layer.index] = solidContainer
+        layerMap[layerIndex] = solidContainer
       } else if let precompLayer = layer as? PreCompLayerModel,
         let assetLibrary = assetLibrary,
         let precompAsset = assetLibrary.precompAssets[precompLayer.referenceID] {
@@ -45,31 +46,31 @@ extension Array where Element == LayerModel {
                                                    assetLibrary: assetLibrary,
                                                    frameRate: frameRate)
         compositionLayers.append(precompContainer)
-        layerMap[layer.index] = precompContainer
+        layerMap[layerIndex] = precompContainer
       } else if let imageLayer = layer as? ImageLayerModel,
         let assetLibrary = assetLibrary,
         let imageAsset = assetLibrary.imageAssets[imageLayer.referenceID] {
         let imageContainer = ImageCompositionLayer(imageLayer: imageLayer, size: CGSize(width: imageAsset.width, height: imageAsset.height))
         compositionLayers.append(imageContainer)
-        layerMap[layer.index] = imageContainer
+        layerMap[layerIndex] = imageContainer
       } else if let textLayer = layer as? TextLayerModel {
         let textContainer = TextCompositionLayer(textLayer: textLayer, textProvider: textProvider, fontProvider: fontProvider)
         compositionLayers.append(textContainer)
-        layerMap[layer.index] = textContainer
+        layerMap[layerIndex] = textContainer
       } else {
         let genericLayer = NullCompositionLayer(layer: layer)
         compositionLayers.append(genericLayer)
-        layerMap[layer.index] = genericLayer
+        layerMap[layerIndex] = genericLayer
       }
       if layer.parent != nil {
         childLayers.append(layer)
       }
     }
-    
     /// Now link children with their parents
     for layerModel in childLayers {
+      let layerIndex = layerModel.index ?? Int(layerModel.inFrame)
       if let parentID = layerModel.parent {
-        let childLayer = layerMap[layerModel.index]
+        let childLayer = layerMap[layerIndex]
         let parentLayer = layerMap[parentID]
         childLayer?.transformNode.parentNode = parentLayer?.transformNode
       }
