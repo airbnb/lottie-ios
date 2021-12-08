07b72dd5c1616a00c6a187ea17546daa81af7461
Cal Stephens <cal.stephens@airbnb.com>
2021-12-07 16:20:54 -0800

    Run linter on Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
   // platforms: [.iOS("9.0"), .macOS("10.10"), tvOS("9.0"), .watchOS("2.0")],
   platforms: [.iOS(.v9)],
   products: [
-    .library(name: "Lottie", targets: ["Lottie"])
+    .library(name: "Lottie", targets: ["Lottie"]),
   ],
   targets: [
     .target(name: "Lottie", path: "Sources", exclude: ["Public/MacOS"]),
