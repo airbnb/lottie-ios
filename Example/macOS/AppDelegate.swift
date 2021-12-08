7c953e2a5c8e0a23ca1c2876c51da94ffac7aeeb
Cal Stephens <cal.stephens@airbnb.com>
2021-12-08 10:05:25 -0800

    Set up working targets for the macOS and tvOS Example apps

--- Example/macOS/AppDelegate.swift
+++ Example/macOS/AppDelegate.swift
@@ -11,12 +11,4 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-  func applicationDidFinishLaunching(_: Notification) {
-    // Insert code here to initialize your application
-  }
-
-  func applicationWillTerminate(_: Notification) {
-    // Insert code here to tear down your application
-  }
-
 }
