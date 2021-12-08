f814d909e0661ff7995c35a896fefd0765c06cc5
Cal Stephens <cal.stephens@airbnb.com>
2021-12-08 09:44:45 -0800

    Support macOS and tvOS in Package.swift

--- Package.swift
+++ Package.swift
@@ -3,10 +3,7 @@ import PackageDescription
 
 let package = Package(
   name: "Lottie",
-  // TODO: Fix SPM support for macOS, tvOS, and watchOS.
-  // https://github.com/airbnb/lottie-ios/issues/1361
-  // platforms: [.iOS("9.0"), .macOS("10.10"), tvOS("9.0"), .watchOS("2.0")],
-  platforms: [.iOS(.v9)],
+  platforms: [.iOS("9.0"), .macOS("10.10"), .tvOS("9.0")],
   products: [
     .library(name: "Lottie", targets: ["Lottie"]),
   ],
