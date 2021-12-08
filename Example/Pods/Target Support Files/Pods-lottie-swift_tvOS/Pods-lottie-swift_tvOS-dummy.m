920c2ce5b2ec6942d9b13df674b45be570d1f711
Cal Stephens <cal.stephens@airbnb.com>
2021-12-07 16:07:17 -0800

    Update directory structure, create top-level xcworkspace that uses SPM

--- Example/Pods/Target Support Files/Pods-lottie-swift_tvOS/Pods-lottie-swift_tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_lottie_swift_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_lottie_swift_tvOS
-@end
--- Example/Pods/Target Support Files/lottie-ios-iOS/lottie-ios-iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_lottie_ios_iOS : NSObject
-@end
-@implementation PodsDummy_lottie_ios_iOS
-@end
