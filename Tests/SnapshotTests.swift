71351361070b42165c06f0821ff15261e6634284
Cal Stephens <cal.stephens@airbnb.com>
2021-12-15 13:02:33 -0800

    Update tests, generate new snapshots

--- Tests/SnapshotTests.swift
+++ Tests/SnapshotTests.swift
@@ -69,7 +69,7 @@ class SnapshotTests: XCTestCase {
 
     /// Custom configurations for individual snapshot tests that
     /// cannot use the default configuration
-    static let customMapping: [SnapshotConfiguration] = [
+    static let customMapping = [
       /// These samples appear to render in a slightly non-deterministic way,
       /// depending on the test environment, so we have to decrease precision a bit.
       "issue_1407": SnapshotConfiguration(precision: 0.9),
