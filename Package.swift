f237d87961d78c08baefd2e134b72fd07eb5c98b
Cal Stephens <cal@calstephens.tech>
2021-12-21 09:21:56 -0800

    Rename Carthage framework from "LottieCarthage" to just "Lottie" (#1454)

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ let package = Package(
   platforms: [.iOS("11.0"), .macOS("10.10"), .tvOS("11.0")],
   products: [
     .library(name: "Lottie", targets: ["Lottie"]),
+    .library(name: "_LottieCore", targets: ["LottieCore"]),
   ],
   dependencies: [
     .package(
@@ -14,7 +15,20 @@ let package = Package(
       .revision("0259c2fbd69c7bfaa34b0b86280496f84653e396")),
   ],
   targets: [
-    .target(name: "Lottie", path: "Sources"),
+    .target(
+      name: "Lottie",
+      dependencies: ["LottieCore"],
+      path: "Sources",
+      exclude: ["Public", "Private"]),
+
+    // The primary library target that contains the Lottie package
+    //  - This target is imported and re-exported by the "Lottie" SPM target
+    //    and the "Lottie" Carthage framework.
+    .target(
+      name: "LottieCore",
+      path: "Sources",
+      exclude: ["Lottie.swift"]),
+
     .testTarget(
       name: "LottieTests",
       dependencies: ["Lottie", "SnapshotTesting"],
--- lottie-ios.podspec
+++ lottie-ios.podspec
@@ -28,6 +28,7 @@ Lottie enables designers to create and ship beautiful animations without an engi
   s.tvos.deployment_target = '11.0'
 
   s.source_files = 'Sources/**/*'
+  s.exclude_files = 'Sources/Lottie.swift'
   s.ios.exclude_files = 'Sources/Public/MacOS/**/*'
   s.tvos.exclude_files = 'Sources/Public/MacOS/**/*'
   s.osx.exclude_files = 'Sources/Public/iOS/**/*'
