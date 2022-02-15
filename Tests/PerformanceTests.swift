541104d5088d4246fe204a7293801631417fbb58
Cal Stephens <cal.stephens@airbnb.com>
2022-02-14 17:34:37 -0800

    Add tests to compare setup performance of Core Animation engine with Main Thread engine

---
+++ Tests/PerformanceTests.swift
@@ -0,0 +1,124 @@
+// Created by Cal Stephens on 2/14/22.
+// Copyright © 2022 Airbnb Inc. All rights reserved.
+
+import Foundation
+import XCTest
+
+@testable import Lottie
+
+// MARK: - PerformanceTests
+
+final class PerformanceTests: XCTestCase {
+
+  // MARK: Internal
+
+  func testAnimationViewSetup_simpleAnimation() {
+    // Compare the performance of displaying this simple animation in the two animation engines
+    let ratio = compareEngineSetupPerformance(for: simpleAnimation, iterations: 2000)
+
+    // This is basically a snapshot test for the performance of the Core Animation engine
+    // compared to the Main Thread engine. Currently, the Core Animation engine is
+    // about 2.2x slower than the Main Thread engine in this example.
+    XCTAssertEqual(ratio, 2.2, accuracy: 0.2)
+  }
+
+  func testAnimationViewSetup_complexAnimation() {
+    let ratio = compareEngineSetupPerformance(for: complexAnimation, iterations: 500)
+    XCTAssertEqual(ratio, 3.1, accuracy: 0.3)
+  }
+
+  func testAnimationViewScrubbing_simpleAnimation() {
+    let ratio = compareEngineScrubbingPerformance(for: simpleAnimation, iterations: 1000)
+    XCTAssertEqual(ratio, 0.05, accuracy: 0.02)
+  }
+
+  func testAnimationViewScrubbing_complexAnimation() {
+    let ratio = compareEngineScrubbingPerformance(for: complexAnimation, iterations: 1000)
+    XCTAssertEqual(ratio, 0.002, accuracy: 0.001)
+  }
+
+  // MARK: Private
+
+  private let simpleAnimation = Animation.named(
+    "loading_dots_1",
+    bundle: .module,
+    subdirectory: "Samples/LottieFiles")!
+
+  private let complexAnimation = Animation.named(
+    "LottieLogo2",
+    bundle: .module,
+    subdirectory: "Samples")!
+
+  /// Compares initializing the given animation with both the Main Thread and Core Animation engine,
+  /// and returns the ratio of how much slower the Core Animation is than the Main Thread engine
+  private func compareEngineSetupPerformance(for animation: Animation, iterations: Int) -> Double {
+    let mainThreadEnginePerformance = measurePerformance {
+      for _ in 0 ..< iterations {
+        setupAnimationView(with: animation, configuration: .init(renderingEngine: .mainThread))
+      }
+    }
+
+    print("Main thread engine took \(mainThreadEnginePerformance) seconds")
+
+    let coreAnimationEnginePerformance = measurePerformance {
+      for _ in 0 ..< iterations {
+        setupAnimationView(with: animation, configuration: .init(renderingEngine: .coreAnimation))
+      }
+    }
+
+    print("Core Animation engine took \(coreAnimationEnginePerformance) seconds")
+
+    let ratio = coreAnimationEnginePerformance / mainThreadEnginePerformance
+    print("Core Animation engine is \(ratio)x slower than the Main Thread engine")
+    return ratio
+  }
+
+  /// Compares performance of scrubbing the given animation with both the Main Thread and Core Animation engine,
+  /// and returns the ratio of how much slower the Core Animation is than the Main Thread engine
+  private func compareEngineScrubbingPerformance(for animation: Animation, iterations: Int) -> Double {
+    let mainThreadAnimationView = setupAnimationView(with: animation, configuration: .init(renderingEngine: .mainThread))
+    let mainThreadEnginePerformance = measurePerformance {
+      for i in 0 ..< iterations {
+        mainThreadAnimationView.currentProgress = Double(i) / Double(iterations)
+
+        // Since the main thread engine only re-renders in `display()`, which is normally called by Core Animation,
+        // we have to invoke it manually. This triggers the same code path that would happen when scrubbing in
+        // a real use case.
+        mainThreadAnimationView.animationLayer!.display()
+      }
+    }
+
+    print("Main thread engine took \(mainThreadEnginePerformance) seconds")
+
+    let coreAnimationView = setupAnimationView(with: animation, configuration: .init(renderingEngine: .coreAnimation))
+    let coreAnimationEnginePerformance = measurePerformance {
+      for i in 0 ..< iterations {
+        coreAnimationView.currentProgress = Double(i) / Double(iterations)
+      }
+    }
+
+    print("Core Animation engine took \(coreAnimationEnginePerformance) seconds")
+
+    let ratio = coreAnimationEnginePerformance / mainThreadEnginePerformance
+    print("Core Animation engine is \(ratio)x slower than the Main Thread engine")
+    return ratio
+  }
+
+  @discardableResult
+  private func setupAnimationView(with animation: Animation, configuration: LottieConfiguration) -> AnimationView {
+    let animationView = AnimationView(animation: animation, configuration: configuration)
+    animationView.frame.size = CGSize(width: animation.width, height: animation.height)
+    animationView.layoutIfNeeded()
+    animationView.currentProgress = 0.5
+    return animationView
+  }
+
+  private func measurePerformance(_ block : () -> Void) -> TimeInterval {
+    let start = DispatchTime.now()
+    block()
+    let end = DispatchTime.now()
+    let nanoTime = end.uptimeNanoseconds - start.uptimeNanoseconds
+    return Double(nanoTime) / 1_000_000_000
+  }
+
+}
