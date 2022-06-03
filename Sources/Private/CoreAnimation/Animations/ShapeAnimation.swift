f5e79df155084d9837eb05d0266430712b923654
JT Bergman <61445278+jtbergman@users.noreply.github.com>
2022-06-02 22:24:01 -0400

    Add partial support for trim offsets

--- Sources/Private/CoreAnimation/Animations/ShapeAnimation.swift
+++ Sources/Private/CoreAnimation/Animations/ShapeAnimation.swift
@@ -47,13 +47,7 @@ extension CAShapeLayer {
   /// Adds animations for `strokeStart` and `strokeEnd` from the given `Trim` object
   @nonobjc
   func addAnimations(for trim: Trim, context: LayerAnimationContext) throws {
-    let (strokeStartKeyframes, strokeEndKeyframes) = trim.caShapeLayerKeyframes()
-
-    if trim.offset.keyframes.contains(where: { $0.value.cgFloatValue != 0 }) {
-      try context.logCompatibilityIssue("""
-        The CoreAnimation rendering engine doesn't support Trim offsets
-        """)
-    }
+    let (strokeStartKeyframes, strokeEndKeyframes) = try trim.caShapeLayerKeyframes(context: context)
 
     try addAnimation(
       for: .strokeStart,
@@ -82,19 +76,51 @@ extension Trim {
   // MARK: Fileprivate
 
   /// The `strokeStart` and `strokeEnd` keyframes to apply to a `CAShapeLayer`
-  ///  - `CAShapeLayer` requires that `strokeStart` be less than `strokeEnd`.
-  ///  - Since this isn't a requirement in the Lottie schema, there are
-  ///    some animations that have `strokeStart` and `strokeEnd` flipped.
-  ///  - If we detect that this is the case for this specific `Trim`, then
-  ///    we swap the start/end keyframes to match what `CAShapeLayer` expects.
-  fileprivate func caShapeLayerKeyframes()
+  fileprivate func caShapeLayerKeyframes(context: LayerAnimationContext) throws
     -> (strokeStart: KeyframeGroup<Vector1D>, strokeEnd: KeyframeGroup<Vector1D>)
   {
+    let strokeStart: KeyframeGroup<Vector1D>
+    let strokeEnd: KeyframeGroup<Vector1D>
+    
+    // CAShapeLayer requires strokeStart to be less than strokeEnd. This
+    // isn't required by the Lottie schema, so some animations may have
+    // strokeStart and strokeEnd flipped. If we detect this is the case,
+    // then swap them.
     if startValueIsAlwaysGreaterThanEndValue() {
-      return (strokeStart: end, strokeEnd: start)
+      strokeStart = end
+      strokeEnd = start
     } else {
-      return (strokeStart: start, strokeEnd: end)
+      strokeStart = start
+      strokeEnd = end
     }
+    
+    // Adjust the keyframes to account for trim offsets if possible
+    let adjustedStrokeStart = try adjustKeyframesForTrimOffsets(
+      strokeKeyframes: strokeStart.keyframes,
+      offsetKeyframes: offset.keyframes,
+      context: context
+    )
+    
+    let adjustedStrokeEnd = try adjustKeyframesForTrimOffsets(
+      strokeKeyframes: strokeEnd.keyframes,
+      offsetKeyframes: offset.keyframes,
+      context: context
+    )
+    
+    // Validate the adjusted keyframes and fallback on original if invalid
+    if (adjustedStrokeStart + adjustedStrokeEnd).contains(where: {
+      $0.value.cgFloatValue < 0 || $0.value.cgFloatValue > 100
+    }) {
+      try context.logCompatibilityIssue("""
+        The CoreAnimation rendering engine doesn't support Trim offsets
+        """)
+      return (strokeStart: strokeStart, strokeEnd: strokeEnd)
+    }
+    
+    return (
+      strokeStart: KeyframeGroup<Vector1D>(keyframes: adjustedStrokeStart),
+      strokeEnd: KeyframeGroup<Vector1D>(keyframes: adjustedStrokeEnd)
+    )
   }
 
   // MARK: Private
@@ -119,5 +145,85 @@ extension Trim {
     }
 
     return true
+  }
+  
+  /// Adjusts a `KeyframeGroup` associated with a `strokeStart` or `strokeEnd` to account
+  /// for trim offsets. The CoreAnimation rendering engine does not fully support trim offsets. If applying
+  /// the offsets results in a `KeyframeGroup` with all values in the range `[0, 1]` then the offsets
+  /// can be supported.
+  private func adjustKeyframesForTrimOffsets(
+    strokeKeyframes: ContiguousArray<Keyframe<Vector1D>>,
+    offsetKeyframes: ContiguousArray<Keyframe<Vector1D>>,
+    context: LayerAnimationContext
+  ) throws -> ContiguousArray<Keyframe<Vector1D>> {
+    guard
+      !offsetKeyframes.isEmpty,
+      offsetKeyframes.contains(where: { $0.value.cgFloatValue != 0 })
+    else {
+      return strokeKeyframes
+    }
+    
+    // Map each keyframe time to its associated stroke/offset
+    var timeMap = [AnimationFrameTime: [Keyframe<Vector1D>?]]()
+    for stroke in strokeKeyframes {
+      timeMap[stroke.time] = [stroke, nil]
+    }
+    for offset in offsetKeyframes {
+      if var existing = timeMap[offset.time] {
+        existing[1] = offset
+        timeMap[offset.time] = existing
+      } else {
+        timeMap[offset.time] = [nil, offset]
+      }
+    }
+    
+    // Each time will be mapped to a new, adjusted keyframe
+    var output = ContiguousArray<Keyframe<Vector1D>>()
+    var lastKeyframe: Keyframe<Vector1D>?
+    var lastOffset: Keyframe<Vector1D>?
+    for (time, values) in timeMap.sorted(by: { $0.0 < $1.0 }) {
+      // Extract keyframe/offset associated with this timestamp
+      let keyframe = values[0]
+      let offset = values[1]
+      lastKeyframe = keyframe ?? lastKeyframe
+      lastOffset = offset ?? lastOffset
+      
+      guard let currentKeyframe = lastKeyframe else {
+        // No keyframes are output until the first keyframe occurs
+        continue
+      }
+
+      guard let currentOffset = lastOffset else {
+        // Scalar isHold keyframes are not output as they offset the offset keyframes
+        if !(strokeKeyframes.count == 1 && currentKeyframe.isHold) {
+          output.append(currentKeyframe)
+        }
+        continue
+      }
+      
+      // Compute the adjusted value by converting the offset value to a stroke value
+      let strokeValue = currentKeyframe.value.value
+      let offsetValue = currentOffset.value.value
+      let adjustedValue = strokeValue + (offsetValue / 360 * 100)
+          
+      // Create the adjusted keyframe using the properties of the most recent keyframe
+      let keyframePropertiesToUse = currentKeyframe.time >= currentOffset.time
+        ? currentKeyframe
+        : currentOffset
+      
+      let adjustedKeyframe = Keyframe<Vector1D>(
+        value: Vector1D(adjustedValue),
+        time: time,
+        isHold: keyframePropertiesToUse.isHold,
+        inTangent: keyframePropertiesToUse.inTangent,
+        outTangent: keyframePropertiesToUse.outTangent,
+        spatialInTangent: keyframePropertiesToUse.spatialInTangent,
+        spatialOutTangent: keyframePropertiesToUse.spatialOutTangent
+      )
+      
+      output.append(adjustedKeyframe)
+    }
+          
+    return output
   }
 }
