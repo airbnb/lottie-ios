1baa3b1459803c8b8473aeb96a5187e2a9d0ef39
Jasdev Singh <jasdevs@gmail.com>
2022-06-27 17:26:56 -0400

    `NSImage.CGImage` ⇒ `.lottie_CGImage` to avoid category warning.

--- Sources/Public/macOS/FilepathImageProvider.macOS.swift
+++ Sources/Public/macOS/FilepathImageProvider.macOS.swift
@@ -35,18 +35,18 @@ public class FilepathImageProvider: AnimationImageProvider {
       let data = try? Data(contentsOf: url),
       let image = NSImage(data: data)
     {
-      return image.CGImage
+      return image.lottie_CGImage
     }
 
     let directPath = filepath.appendingPathComponent(asset.name).path
     if FileManager.default.fileExists(atPath: directPath) {
 
-      return NSImage(contentsOfFile: directPath)?.CGImage
+      return NSImage(contentsOfFile: directPath)?.lottie_CGImage
     }
 
     let pathWithDirectory = filepath.appendingPathComponent(asset.directory).appendingPathComponent(asset.name).path
     if FileManager.default.fileExists(atPath: pathWithDirectory) {
-      return NSImage(contentsOfFile: pathWithDirectory)?.CGImage
+      return NSImage(contentsOfFile: pathWithDirectory)?.lottie_CGImage
     }
 
     LottieLogger.shared.assertionFailure("Could not find image \"\(asset.name)\" in bundle")
@@ -60,7 +60,7 @@ public class FilepathImageProvider: AnimationImageProvider {
 
 extension NSImage {
   @nonobjc
-  var CGImage: CGImage? {
+  var lottie_CGImage: CGImage? {
     guard let imageData = tiffRepresentation else { return nil }
     guard let sourceData = CGImageSourceCreateWithData(imageData as CFData, nil) else { return nil }
     return CGImageSourceCreateImageAtIndex(sourceData, 0, nil)
