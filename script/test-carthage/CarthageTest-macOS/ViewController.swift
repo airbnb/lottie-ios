44dad4c04eafeac08b4a4c88975f0e4cea9d9fe9
Cal Stephens <cal.stephens@airbnb.com>
2022-07-08 11:35:39 -0700

    Update carthage test script to test macOS

---
+++ script/test-carthage/CarthageTest-macOS/ViewController.swift
@@ -0,0 +1,16 @@
+// Created by Cal Stephens on 7/8/22.
+// Copyright © 2022 Airbnb Inc. All rights reserved.
+
+import Cocoa
+import Lottie
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    let animationView = AnimationView()
+    view.addSubview(animationView)
+  }
+
+}
+
