e006a56c27e8b9be8c70fe9955bf9299a25f56dc
Cal Stephens <cal.stephens@airbnb.com>
2022-07-20 10:35:36 -0700

    Set up separate Package.swift files for 5.5 and 5.6, since the plugin requires 5.6

--- Package@swift-5.6.swift
+++ Package@swift-5.6.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
@@ -6,6 +6,9 @@ let package = Package(
   platforms: [.iOS("11.0"), .macOS("10.10"), .tvOS("11.0")],
   products: [
     .library(name: "Lottie", targets: ["Lottie"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/airbnb/swift", branch: "master"),
   ],
   targets: [
     .target(name: "Lottie", path: "Sources"),
