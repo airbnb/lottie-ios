6ebb57daa54191ee02d79f3d1f206927a3e83c41
Cal Stephens <cal.stephens@airbnb.com>
2022-07-20 09:56:46 -0700

    Add Airbnb swift style guide repo as dependency

--- Package.swift
+++ Package.swift
@@ -1,8 +1,15 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
   name: "Lottie",
   platforms: [.iOS("11.0"), .macOS("10.10"), .tvOS("11.0")],
-  products: [.library(name: "Lottie", targets: ["Lottie"])],
-  targets: [.target(name: "Lottie", path: "Sources")])
+  products: [
+    .library(name: "Lottie", targets: ["Lottie"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/airbnb/swift", branch: "master"),
+  ],
+  targets: [
+    .target(name: "Lottie", path: "Sources"),
+  ])
