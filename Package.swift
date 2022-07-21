10dbba3b50bd87e9ed3325f69972222848dc9562
Cal Stephens <cal.stephens@airbnb.com>
2022-07-21 08:58:35 -0700

    Use a single Package.swift

--- Package.swift
+++ Package.swift
@@ -4,9 +4,10 @@ import PackageDescription
 let package = Package(
   name: "Lottie",
   platforms: [.iOS("11.0"), .macOS("10.10"), .tvOS("11.0")],
-  products: [
-    .library(name: "Lottie", targets: ["Lottie"]),
-  ],
-  targets: [
-    .target(name: "Lottie", path: "Sources"),
-  ])
+  products: [.library(name: "Lottie", targets: ["Lottie"])],
+  targets: [.target(name: "Lottie", path: "Sources")])
+
+#if swift(>=5.6)
+// Add the Airbnb Swift formatting plugin if possible
+package.dependencies.append(.package(url: "https://github.com/airbnb/swift", .branch("master")))
+#endif
