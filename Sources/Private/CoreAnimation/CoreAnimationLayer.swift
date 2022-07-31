d286982eeadeadfc8206f3eaea94961ca1f1cf8a
Cal Stephens <cal.stephens@airbnb.com>
2022-07-31 11:35:52 -0700

    Fix issue where `isAnimationPlaying` would be incorrect when using `LottieLoopMode.playOnce` (#1682)

--- Sources/Private/CoreAnimation/CoreAnimationLayer.swift
+++ Sources/Private/CoreAnimation/CoreAnimationLayer.swift
@@ -264,6 +264,11 @@ final class CoreAnimationLayer: BaseAnimationLayer {
 
     let timedProgressAnimation = animationProgressTracker.timed(with: context, for: self)
     timedProgressAnimation.delegate = currentAnimationConfiguration?.animationContext.closure
+
+    // Remove the progress animation once complete so we know when the animation
+    // has finished playing (if it doesn't loop infinitely)
+    timedProgressAnimation.isRemovedOnCompletion = true
+
     add(timedProgressAnimation, forKey: #keyPath(animationProgress))
   }
 
@@ -311,7 +316,7 @@ extension CoreAnimationLayer: RootAnimationLayer {
   var isAnimationPlaying: Bool? {
     switch playbackState {
     case .playing:
-      return true
+      return animation(forKey: #keyPath(animationProgress)) != nil
     case nil, .paused:
       return false
     }
