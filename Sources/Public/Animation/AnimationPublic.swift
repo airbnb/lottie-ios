5965c206c980484e427f980f32af29a9c361697a
John Brunelle <anon4949@gmail.com>
2022-08-26 11:09:09 -0400

    Added ability to use a custom URLSession in the Animation Downloader (#1733)

--- Sources/Public/Animation/AnimationPublic.swift
+++ Sources/Public/Animation/AnimationPublic.swift
@@ -172,13 +172,14 @@ extension Animation {
   ///
   public static func loadedFrom(
     url: URL,
+    session: URLSession = .shared,
     closure: @escaping Animation.DownloadClosure,
     animationCache: AnimationCacheProvider?)
   {
     if let animationCache = animationCache, let animation = animationCache.animation(forKey: url.absoluteString) {
       closure(animation)
     } else {
-      let task = URLSession.shared.dataTask(with: url) { data, _, error in
+      let task = session.dataTask(with: url) { data, _, error in
         guard error == nil, let jsonData = data else {
           DispatchQueue.main.async {
             closure(nil)
