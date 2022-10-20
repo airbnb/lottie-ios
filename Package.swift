377f5e9295e4c63cefc01736f95bf177b9eebe09
magic <evandro.hoffmann@gmail.com>
2022-10-20 08:23:54 -0300

    [CHORE] Added ZIPFoundation as a dependency

--- Package.swift
+++ Package.swift
@@ -5,4 +5,5 @@ let package = Package(
   name: "Lottie",
   platforms: [.iOS("11.0"), .macOS("10.10"), .tvOS("11.0")],
   products: [.library(name: "Lottie", targets: ["Lottie"])],
-  targets: [.target(name: "Lottie", path: "Sources")])
+  dependencies: [.package(url: "https://github.com/weichsel/ZIPFoundation.git", .upToNextMajor(from: "0.9.0"))],
+  targets: [.target(name: "Lottie", dependencies: ["ZIPFoundation"], path: "Sources")])
