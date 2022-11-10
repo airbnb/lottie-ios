be6612ae7b90419a1960b25784f28bb6c0ec0bec
rasberik <ras.suleimenov@mercari.com>
2022-11-10 13:01:41 +0900

    Separate tests

--- Tests/AnimationReadyTests.swift
+++ Tests/AnimationReadyTests.swift
@@ -5,8 +5,17 @@ import XCTest
 @MainActor
 final class AnimationReadyTests: XCTestCase {
 
+    func testLottieAnimationViewPlayFailure() {
+        let animationView = setupAnimationView()
+        XCTAssertFalse(animationView.isAnimationPlaying)
+
+        animationView.play()
+        XCTAssertFalse(animationView.isAnimationPlaying)
+        XCTAssertNil(animationView.animation)
+    }
+
     func testAnimationReadyExpectedBehavior() {
-        let animationView = setupAnimationView(dotLottieFilename: "animation_external_image")
+        let animationView = setupAnimationView()
         XCTAssertFalse(animationView.isAnimationPlaying)
 
         animationView.onAnimationReady { view in
@@ -14,15 +23,10 @@ final class AnimationReadyTests: XCTestCase {
             XCTAssertTrue(animationView.isAnimationPlaying)
             XCTAssertNotNil(animationView.animation)
         }
-
-        animationView.play()
-        // This is not expected behavior
-        XCTAssertFalse(animationView.isAnimationPlaying)
-        XCTAssertNil(animationView.animation)
     }
 
     @discardableResult
-    private func setupAnimationView(dotLottieFilename: String) -> LottieAnimationView {
+    private func setupAnimationView(dotLottieFilename: String = "animation_external_image") -> LottieAnimationView {
         let animationView = LottieAnimationView(dotLottieName: dotLottieFilename)
         animationView.frame.size = CGSize(width: 100, height: 100)
         animationView.layoutIfNeeded()
