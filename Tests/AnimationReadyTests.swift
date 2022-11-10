7a44fa1b98ed1c1a49758061bf426c90004d11b2
rasberik <ras.suleimenov@mercari.com>
2022-11-10 16:13:29 +0900

    Update and include tests

--- Tests/AnimationReadyTests.swift
+++ Tests/AnimationReadyTests.swift
@@ -7,33 +7,64 @@ final class AnimationReadyTests: XCTestCase {
 
   // MARK: Internal
 
-  func testLottieAnimationViewPlayFailure() {
-    let animationView = setupAnimationView()
-    XCTAssertFalse(animationView.isAnimationPlaying)
-
-    animationView.play()
-    XCTAssertFalse(animationView.isAnimationPlaying)
-    XCTAssertNil(animationView.animation)
+  func test_dotLottieFile_hasAnimation_onInitialize() {
+    XCTExpectFailure("This is expected failure due to loading delay")
+    let animationView = prepareDotLottieAnimationView()
+    XCTAssertNotNil(animationView.animation)
   }
 
-  func testAnimationReadyExpectedBehavior() {
-    let animationView = setupAnimationView()
-    XCTAssertFalse(animationView.isAnimationPlaying)
+  func test_dotLottieFile_hasAnimation_onAnimationReady() {
+    let exp = XCTestExpectation(description: "onAnimationReady expectation")
+    exp.expectedFulfillmentCount = 1
 
+    let animationView = prepareDotLottieAnimationView()
     animationView.onAnimationReady { _ in
-      animationView.play()
-      XCTAssertTrue(animationView.isAnimationPlaying)
       XCTAssertNotNil(animationView.animation)
+      exp.fulfill()
     }
+    wait(for: [exp], timeout: 0.1)
+  }
+
+  func test_jsonFile_hasAnimation_onInitialize() {
+    let animationView = prepareJsonAnimationView()
+    XCTAssertNotNil(animationView.animation)
+  }
+
+  func test_jsonFile_hasAnimation_onAnimationReady() {
+    let exp = XCTestExpectation(description: "onAnimationReady expectation")
+    exp.expectedFulfillmentCount = 1
+
+    let animationView = prepareJsonAnimationView()
+    animationView.onAnimationReady { _ in
+      XCTAssertNotNil(animationView.animation)
+      exp.fulfill()
+    }
+
+    wait(for: [exp], timeout: 0.1)
   }
 
   // MARK: Private
+  private func prepareDotLottieAnimationView(filename: String = "animation_inline_image") -> LottieAnimationView {
+    guard let fileUrl = Bundle.module.fileURLs(in: Samples.directoryName, withSuffix: "lottie").first(where: { $0.lastPathComponent.hasPrefix(filename)}) else {
+      XCTFail("Lottie file not found: \(filename)")
+      return LottieAnimationView()
+    }
 
-  @discardableResult
-  private func setupAnimationView(dotLottieFilename: String = "animation_external_image") -> LottieAnimationView {
-    let animationView = LottieAnimationView(dotLottieName: dotLottieFilename)
+    let animationView = LottieAnimationView(dotLottieFilePath: fileUrl.path)
+    animationView.frame.size = CGSize(width: 100, height: 100)
+    animationView.layoutIfNeeded()
+    return animationView
+  }
+
+  private func prepareJsonAnimationView(filename: String = "Boat_Loader") -> LottieAnimationView {
+    guard let fileUrl = Bundle.module.fileURLs(in: Samples.directoryName, withSuffix: "json").first(where: { $0.lastPathComponent.hasPrefix(filename)}) else {
+      XCTFail("Json file not found: \(filename)")
+      return LottieAnimationView()
+    }
+    let animationView = LottieAnimationView(filePath: fileUrl.path)
     animationView.frame.size = CGSize(width: 100, height: 100)
     animationView.layoutIfNeeded()
     return animationView
   }
 }
+
