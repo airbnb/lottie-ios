9ce485d7b519a275628adb3667b483d5660524ff
Evandro Harrison Hoffmann <evandro.hoffmann@gmail.com>
2022-12-09 14:47:48 -0300

    Made DotLottieAnimation attributes optional to simplify Manifest file (#1844)

--- Sources/Private/Model/DotLottie/DotLottieAnimation.swift
+++ Sources/Private/Model/DotLottie/DotLottieAnimation.swift
@@ -12,13 +12,10 @@ struct DotLottieAnimation: Codable {
   var id: String
 
   /// Loop enabled
-  var loop: Bool
-
-  // appearance color in HEX
-  var themeColor: String
+  var loop: Bool? = false
 
   /// Animation Playback Speed
-  var speed: Double
+  var speed: Double? = 1
 
   /// 1 or -1
   var direction: Int? = 1
@@ -31,12 +28,12 @@ struct DotLottieAnimation: Codable {
 
   /// Loop mode for animation
   var loopMode: LottieLoopMode {
-    mode == "bounce" ? .autoReverse : (loop ? .loop : .playOnce)
+    mode == "bounce" ? .autoReverse : ((loop ?? false) ? .loop : .playOnce)
   }
 
   /// Animation speed
   var animationSpeed: Double {
-    speed * Double(direction ?? 1)
+    (speed ?? 1) * Double(direction ?? 1)
   }
 
   /// Loads `LottieAnimation` from `animationUrl`
