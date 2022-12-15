ef24f3b3f61d8d2cf3c098df66b2aab9d435554c
Antti Laitala <antti.laitala@ouraring.com>
2022-12-15 09:03:35 +0200

    Fix heap buffer overflow in CGColor+RGB extensions

--- Sources/Private/Utility/Extensions/CGColor+RGB.swift
+++ Sources/Private/Utility/Extensions/CGColor+RGB.swift
@@ -6,22 +6,20 @@ import QuartzCore
 extension CGColor {
   /// Initializes a `CGColor` using the given `RGB` values
   static func rgb(_ red: CGFloat, _ green: CGFloat, _ blue: CGFloat) -> CGColor {
-    CGColor(
-      colorSpace: CGColorSpaceCreateDeviceRGB(),
-      components: [red, green, blue])!
-      .copy(alpha: 1)!
+    rgba(red, green, blue, 1.0)
   }
 
   /// Initializes a `CGColor` using the given grayscale value
   static func gray(_ gray: CGFloat) -> CGColor {
     CGColor(
       colorSpace: CGColorSpaceCreateDeviceGray(),
-      components: [gray])!
-      .copy(alpha: 1)!
+      components: [gray, 1.0])!
   }
 
   /// Initializes a `CGColor` using the given `RGBA` values
   static func rgba(_ red: CGFloat, _ green: CGFloat, _ blue: CGFloat, _ alpha: CGFloat) -> CGColor {
-    CGColor.rgb(red, green, blue).copy(alpha: alpha)!
+    CGColor(
+      colorSpace: CGColorSpaceCreateDeviceRGB(),
+      components: [red, green, blue, alpha])!
   }
 }
