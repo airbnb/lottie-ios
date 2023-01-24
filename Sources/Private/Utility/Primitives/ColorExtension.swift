adda6ed625572745c98922c433a5434cfc5dfc1b
Cal Stephens <cal.stephens@airbnb.com>
2023-01-23 17:14:46 -0800

    Update color parsing to ignore alpha values for consistency with other platforms (#1934)

--- Sources/Private/Utility/Primitives/ColorExtension.swift
+++ Sources/Private/Utility/Primitives/ColorExtension.swift
@@ -38,22 +38,18 @@ extension LottieColor: Codable {
       b1 = 0
     }
 
-    var a1: Double
-    if !container.isAtEnd {
-      a1 = try container.decode(Double.self)
-    } else {
-      a1 = 1
-    }
-    if r1 > 1, g1 > 1, b1 > 1, a1 > 1 {
+    if r1 > 1, g1 > 1, b1 > 1 {
       r1 = r1 / 255
       g1 = g1 / 255
       b1 = b1 / 255
-      a1 = a1 / 255
     }
     r = r1
     g = g1
     b = b1
-    a = a1
+
+    // The Lottie JSON schema supports alpha values in theory, as the fourth value in this array.
+    // We intentionally do not support this, though, for consistency with Lottie on other platforms.
+    a = 1
   }
 
   // MARK: Public
@@ -79,17 +75,18 @@ extension LottieColor: AnyInitializable {
     var r: Double = array.count > 0 ? array.removeFirst() : 0
     var g: Double = array.count > 0 ? array.removeFirst() : 0
     var b: Double = array.count > 0 ? array.removeFirst() : 0
-    var a: Double = array.count > 0 ? array.removeFirst() : 1
-    if r > 1, g > 1, b > 1, a > 1 {
+    if r > 1, g > 1, b > 1 {
       r /= 255
       g /= 255
       b /= 255
-      a /= 255
     }
     self.r = r
     self.g = g
     self.b = b
-    self.a = a
+
+    // The Lottie JSON schema supports alpha values in theory, as the fourth value in this array.
+    // We intentionally do not support this, though, for consistency with Lottie on other platforms.
+    a = 1
   }
 
 }
