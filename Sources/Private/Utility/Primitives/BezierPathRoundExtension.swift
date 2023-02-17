951be44391283253d1576d8dc90683dc1d1c46d3
Cal Stephens <cal.stephens@airbnb.com>
2023-02-17 13:24:33 -0800

    Fix issue where rounded corners algorithm would return incorrect path for shapes without any rounded corners (#1964)

--- Sources/Private/Utility/Primitives/BezierPathRoundExtension.swift
+++ Sources/Private/Utility/Primitives/BezierPathRoundExtension.swift
@@ -83,6 +83,8 @@ extension BezierPath {
       uniquePath.addVertex(elements[i].vertex)
     }
 
+    var pathHasRoundedCorner = false
+
     for elementIndex in 0..<uniquePath.elements.count {
       currentVertex = uniquePath.elements[elementIndex].vertex
 
@@ -95,6 +97,9 @@ extension BezierPath {
         newPath.addVertex(currentVertex)
         continue
       }
+
+      // Track whether or not this path has at least one rounded corner
+      pathHasRoundedCorner = true
 
       // Do not round start and end if not closed
       if !newPath.closed, elementIndex == 0 || elementIndex == uniquePath.elements.count - 1 {
@@ -140,6 +145,12 @@ extension BezierPath {
             CGPoint(x: vX, y: vY),
             CGPoint(x: oX, y: oY)))
       }
+    }
+
+    // If we didn't need to apply the corner radius to any of the corners,
+    // just use the original given path instead of modifying it.
+    if !pathHasRoundedCorner {
+      return self
     }
 
     return newPath
