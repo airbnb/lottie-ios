8b89a433e5ca71e9447352d56cfd4231b38f23f8
Cal Stephens <cal.stephens@airbnb.com>
2023-08-10 09:18:39 -0700

    Fix unhandled file warning when building with SPM (#2132)

--- Package.swift
+++ Package.swift
@@ -6,7 +6,16 @@ let package = Package(
   // Minimum platform versions should be kept in sync with the per-platform targets in Lottie.xcodeproj, lottie-ios.podspec, and lottie-spm's Package.swift
   platforms: [.iOS("11.0"), .macOS("10.11"), .tvOS("11.0")],
   products: [.library(name: "Lottie", targets: ["Lottie"])],
-  targets: [.target(name: "Lottie", path: "Sources")])
+  targets: [
+    .target(
+      name: "Lottie",
+      path: "Sources",
+      exclude: [
+        "Private/EmbeddedLibraries/README.md",
+        "Private/EmbeddedLibraries/ZipFoundation/README.md",
+        "Private/EmbeddedLibraries/EpoxyCore/README.md",
+      ]),
+  ])
 
 #if swift(>=5.6)
 // Add the Airbnb Swift formatting plugin if possible
