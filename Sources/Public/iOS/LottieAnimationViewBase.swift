f081afab0e4da9c0ae54e1db1c0f30b603f3eb03
Hyungyu Kim <ypsophila97@icloud.com>
2023-11-02 22:30:51 +0900

    Use `UITraitCollection.current.displayScale` instead of deprecated `UIScreen.main.scale` (#2216)
    Co-authored-by: Cal Stephens <cal@calstephens.tech>

--- Sources/Public/iOS/LottieAnimationViewBase.swift
+++ Sources/Public/iOS/LottieAnimationViewBase.swift
@@ -39,7 +39,11 @@ open class LottieAnimationViewBase: UIView {
 
   var screenScale: CGFloat {
     #if os(iOS) || os(tvOS)
-    UIScreen.main.scale
+    if #available(iOS 13.0, tvOS 13.0, *) {
+      return max(UITraitCollection.current.displayScale, 1)
+    } else {
+      return UIScreen.main.scale
+    }
     #else // if os(visionOS)
     // We intentionally don't check `#if os(visionOS)`, because that emits
     // a warning when building on Xcode 14 and earlier.
@@ -79,6 +83,5 @@ open class LottieAnimationViewBase: UIView {
   func animationWillEnterForeground() {
     // Implemented by subclasses.
   }
-
 }
 #endif
