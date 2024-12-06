55aa1dba025668cd56ec8ca8f4ce3403479155b3
Jens Krug <jens.k611@googlemail.com>
2024-12-06 15:39:57 +0100

    Add additional dynamic library product to Package.swift (#2507)

--- Package.swift
+++ Package.swift
@@ -5,7 +5,12 @@ let package = Package(
   name: "Lottie",
   // Minimum platform versions should be kept in sync with the per-platform targets in Lottie.xcodeproj, lottie-ios.podspec, and lottie-spm's Package.swift
   platforms: [.iOS("13.0"), .macOS("10.15"), .tvOS("13.0"), .custom("visionOS", versionString: "1.0")],
-  products: [.library(name: "Lottie", targets: ["Lottie"])],
+  products: [
+    .library(name: "Lottie", targets: ["Lottie"]),
+    // Product for dynamic linking, as SPM builds libraries statically by default.
+    // This product should be used in a modular project structure with multiple packages using Lottie to avoid duplicate symbols.
+    .library(name: "Lottie-Dynamic", type: .dynamic, targets: ["Lottie"]),
+  ],
   dependencies: [
     .package(url: "https://github.com/airbnb/swift", .upToNextMajor(from: "1.0.1")),
   ],
