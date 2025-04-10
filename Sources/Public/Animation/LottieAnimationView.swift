81ca7a1ec860e644a3ac76fc6786cb77c97dc430
joy <yanglu@fengjikeji.com>
2025-04-10 10:21:25 +0800

    add

--- Sources/Public/Animation/LottieAnimationView.swift
+++ Sources/Public/Animation/LottieAnimationView.swift
@@ -683,6 +683,7 @@ open class LottieAnimationView: LottieAnimationViewBase {
     }
 
     
+    
     /// Reads the value of a property specified by the Keypath.
     /// Returns nil if no property is found.
     ///
