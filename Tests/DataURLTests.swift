2c5fda5ac4bb79da3b63139ea2a520514a8ee3fa
Hashim Khan <64767361+Hashim1999164@users.noreply.github.com>
2026-09-02 20:44:57 +0500

    Expect base64 decode to proceed when available memory is zero.

--- Tests/DataURLTests.swift
+++ Tests/DataURLTests.swift
@@ -79,10 +79,10 @@ final class DataURLTests: XCTestCase {
     XCTAssertNotNil(decoder.decode(red5x5Base64))
   }
 
-  func testBase64DecodeSkipsWhenAvailableMemoryIsZero() {
+  func testBase64DecodeProceedsWhenAvailableMemoryIsZero() {
     let decoder = Base64DataURLDecoder(availableMemoryByteCount: { 0 })
 
-    XCTAssertNil(decoder.decode(red5x5Base64))
+    XCTAssertNotNil(decoder.decode(red5x5Base64))
   }
 
   func testBase64DecodeProceedsWhenAvailableMemoryIsUnavailable() {
